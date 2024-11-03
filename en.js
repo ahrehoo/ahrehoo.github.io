490ff73d1dafb0618f14bd4b05d613d10a1f8fd8
ahreho Sec <91535534+ahrehoo@users.noreply.github.com>
2024-11-04 02:54:10 +0330

    latest till now

--- en.js
+++ en.js
@@ -1,4 +1,4 @@
-function setFontVariations(lang){
+function setFontVariations(lang) {
 }
 function setLoginTexts(lang) {
     document.getElementById('login').innerText = "Login";
@@ -14,4 +14,23 @@ function setRegisterTexts(lang) {
     document.getElementById('confirm-pass').placeholder = "Confirm password";
     document.getElementById('phone').placeholder = "Phone";
     document.getElementById('submit').value = "Register";
+}
+function setStatusTexts(lang) {
+    document.getElementById('status').innerText = "Status";
+    document.getElementById('sensors').innerText = "Sensors";
+    document.getElementById('phones').innerText = "Phones";
+    document.getElementById('alarms').innerText = "Alarms";
+    document.getElementById('security').innerText = "Security";
+    document.getElementById('diagnostics').innerText = "Diagnotics";
+    document.getElementById('general').innerText = "General";
+
+    document.getElementById('dropdown-language').innerText = "- Language";
+    document.getElementById('dropdown-user-information').innerText = "- User information";
+    document.getElementById('dropdown-backup-restore').innerText = "- Backup & Restore";
+    document.getElementById('dropdown-update').innerText = "- Update";
+    document.getElementById('dropdown-factory-reset').innerText = "- Factory reset";
+    
+    document.getElementById('quick-setup').innerText = "Quick setup";
+    document.getElementById('simple').innerText = "Simple";
+    document.getElementById('advanced').innerText = "Advanced";
 }
--- fa.js
+++ fa.js
@@ -15,3 +15,18 @@ function setRegisterTexts(lang) {
     document.getElementById('confirm-pass').placeholder = "تایید رمز عبور";
     document.getElementById('submit').value = "ثبت نام";
 }
+function setStatusTexts(lang) {
+    document.getElementById('status').innerText = "وضعیت";
+    document.getElementById('sensors').innerText = "سنسور ها";
+    document.getElementById('phones').innerText = "تلفن ها";
+    document.getElementById('alarms').innerText = "هشدار ها";
+    document.getElementById('security').innerText = "امنیت";
+    document.getElementById('diagnostics').innerText = "بررسی و چکاپ";
+    document.getElementById('general').innerText = "تنظیمات عمومی";
+
+    document.getElementById('dropdown-language').innerText = "زبان";
+    document.getElementById('dropdown-user-information').innerText = "اطلاعات کاربری";
+    document.getElementById('dropdown-backup-restore').innerText = "پشتیبان گیری و بازیابی";
+    document.getElementById('dropdown-update').innerText = "بروزرسانی";
+    document.getElementById('dropdown-factory-reset').innerText = "تنظیمات کارخانه";
+}
