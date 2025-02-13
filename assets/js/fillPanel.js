76776e44e4b4d7d182704df17877fd026ddaaa1f
Ahreho Main <91535534+ahrehoo@users.noreply.github.com>
2025-02-13 21:01:35 +0330

    tr

--- assets/js/fillPanel.js
+++ assets/js/fillPanel.js
@@ -5,12 +5,12 @@ window.addEventListener('DOMContentLoaded', async () => {
 });
 
 async function loadPage(page) {
-    page = page == null ? "dashboard" : page;/*
+    page = page == null ? "dashboard" : page;
     if (window.location.search != "?page=" + page) {
         const urlParams = new URLSearchParams(window.location.search);
         urlParams.set('page', page);
         window.location.search = urlParams;
-    }*/
+    }
     const pageData = await fetchPage(`pages/${page}.html`);
     document.getElementById("container").innerHTML = pageData;
     document.documentElement.setAttribute("page", page);
