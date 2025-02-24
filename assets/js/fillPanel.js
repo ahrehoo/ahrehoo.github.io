a7d82f04ea13f3ccb5624dc081d1baa24c3a274d
Ahreho Main <91535534+ahrehoo@users.noreply.github.com>
2025-02-24 18:49:02 +0330

    tr

--- assets/js/fillPanel.js
+++ assets/js/fillPanel.js
@@ -8,12 +8,12 @@ window.addEventListener('DOMContentLoaded', async () => {
 });
 
 async function loadPage(page) {
-    page = page == null ? "dashboard" : page;/*
+    page = page == null ? "dashboard" : page;
     if (debug && window.location.search != "?page=" + page) {
         const urlParams = new URLSearchParams(window.location.search);
         urlParams.set('page', page);
         window.location.search = urlParams;
-    }*/
+    }
     const lang = await getSetting('lang');
     const pageTData = await fetchJSON(`assets/language/${lang}/${page}.json`);
     const pageData = await fetchPage(`pages/${page}.html`);
